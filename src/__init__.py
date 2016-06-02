95d34fbc36a54867bc084f765283fe11d8185ead
Sotaya <sotayamy@gmail.com>
2016-06-02 13:20:09 +0200

    Initial commit with project structure

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+__author__ = "Sotaya (plaix)"
+__copyright__ = "Copyright 2016, The Trixie Project"
+__credits__ = []
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Sotaya Yakubu"
+__email__ = "sotaya@gmail.com"
+__status__ = "Production"
+
